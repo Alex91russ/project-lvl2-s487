30ce95b3ad4a1a56d06570de3b299efc9828be29
Alex91russ <k_alex91@mail.ru>
2019-06-09 19:57:57 +0400

    add parser for ini

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 import yml from 'js-yaml';
+import ini from 'ini';
 
 const jsonParser = data => JSON.parse(data);
 const ymlParser = date => yml.safeLoad(date);
+const iniParser = date => ini.parse(date);
 
 const parsers = {
   '.json': jsonParser,
   '.yml': ymlParser,
+  '.ini': iniParser,
 };
 
 export default (filePath) => {
