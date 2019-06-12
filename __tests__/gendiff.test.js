dbdd0c14ad75fe4ffc0b1adae6c07107cc93308a
Alex91russ <k_alex91@mail.ru>
2019-06-12 12:55:50 +0400

    add test for filesWithAttachments

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,10 +3,16 @@ import genDiff from '../src';
 
 const path = '__tests__/__fixtures__/';
 const expected = fs.readFileSync(`${path}result.txt`, 'utf8');
+const expected2 = fs.readFileSync(`${path}resultWithAttachments.txt`, 'utf8');
 
 const arrayForTestJson = [
   `${path}json/before.json`,
   `${path}json/after.json`,
+];
+
+const arrayWithAttachForTestJson = [
+  `${path}json/beforeWithAttachments.json`,
+  `${path}json/afterWithAttachments.json`,
 ];
 
 const arrayForTestYml = [
@@ -14,14 +20,31 @@ const arrayForTestYml = [
   `${path}yml/after.yml`,
 ];
 
+const arrayWithAttachForTestYml = [
+  `${path}yml/beforeWithAttachments.yml`,
+  `${path}yml/afterWithAttachments.yml`,
+];
+
 const arrayForTestIni = [
   `${path}ini/before.ini`,
   `${path}ini/after.ini`,
+];
+
+const arrayWithAttachForTestIni = [
+  `${path}ini/beforeWithAttachments.ini`,
+  `${path}ini/afterWithAttachments.ini`,
 ];
 
 test.each([arrayForTestJson, arrayForTestYml, arrayForTestIni])(
   'test for genDiff(%s, %s)',
   (firstPath, secondPath) => {
     expect(genDiff(firstPath, secondPath)).toBe(expected);
+  },
+);
+
+test.each([arrayWithAttachForTestJson, arrayWithAttachForTestYml, arrayWithAttachForTestIni])(
+  'test for genDiff(%s, %s)',
+  (firstPath, secondPath) => {
+    expect(genDiff(firstPath, secondPath)).toBe(expected2);
   },
 );
